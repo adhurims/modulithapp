335450436be0b7b1e103d71dadcbbe606ce1ff91
adhurim-local <adhurim077@gmail.com>
2024-11-02 01:46:47 +0100

    Get version from home

--- Modules/Ordering/Ordering.Domain/obj/Debug/net8.0/Ordering.Domain.AssemblyInfo.cs
+++ Modules/Ordering/Ordering.Domain/obj/Debug/net8.0/Ordering.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05a7ef5d2f4176503fe8433f37b26dbafe4639d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
