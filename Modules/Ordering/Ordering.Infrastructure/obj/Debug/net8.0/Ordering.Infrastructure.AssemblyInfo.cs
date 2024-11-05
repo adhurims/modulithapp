f18100ca8d8f201f0124ef2553973db70a6b1d08
Adhurim Sulejmani <adhurim077@hotmail.com>
2024-11-05 14:01:11 +0100

    //

--- Modules/Ordering/Ordering.Infrastructure/obj/Debug/net8.0/Ordering.Infrastructure.AssemblyInfo.cs
+++ Modules/Ordering/Ordering.Infrastructure/obj/Debug/net8.0/Ordering.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7092cf84cef6b18290d9d38e749eb53c94e7d62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc09c1632b56b3160b9649a06e617de2d0d1ea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
